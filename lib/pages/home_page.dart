5b6f66a32da84a51f6848a46e59e4a5fb73ceef0
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-06 17:01:08 +0100

    Calling the Restaurant Section

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:fast_kaskrot/components/app_bar.dart';
-import 'package:fast_kaskrot/components/bottom_navigation.dart';
+import 'package:fast_kaskrot/components/restaurant_section.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -15,18 +15,24 @@ class HomePage extends StatelessWidget {
                 color: Colors.black,
                 child: const AppBarWidget(barName: 'Home')
             ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 20,horizontal: 10),
+              child: const Text(
+                  'Nearby Restaurants' ,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      letterSpacing: 1
+                  )
+              )
+            ),
+          Column(
+            children: const [
+              RestaurantSection(),
+            ],
+          )
         ],
       ),
-        // bottom: PreferredSize(
-        //     preferredSize: const Size.fromHeight(50),
-        //     child: Row(
-        //       children: [
-        //         Text('data')
-        //       ],
-        //     )
-        // ),
-      );
-
+    );
   }
-
 }
