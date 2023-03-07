8b5472dc08a6aa8534ca21ba7a4b0916f0514202
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-07 12:07:28 +0100

    Calling the RestaurantInfo

--- lib/pages/restaurant_info_page.dart
+++ lib/pages/restaurant_info_page.dart
@@ -1,26 +1,17 @@
-import 'package:fast_kaskrot/components/bottom_navigation.dart';
-import 'package:fast_kaskrot/components/restaurant/restaurant_image_section.dart';
 import 'package:flutter/material.dart';
 
-import '../components/app_bar.dart';
+import '../components/bottom_navigation.dart';
+import '../components/restaurant/restaurant_info_section.dart';
 
-class RestaurantInfo extends StatelessWidget {
-  const RestaurantInfo({super.key});
+
+class RestaurantWidget extends StatelessWidget {
+  const RestaurantWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        children:  [
-          Container(
-              height: 60,
-              color: Colors.black,
-              child:  AppBarWidget(leadingIcon: Icons.arrow_back_outlined,barName: 'Restaurant')
-          ),
-          const RestaurantImage(imageName: 'le_tacos_de_lyon.jpg'),
-        ],
-      ),
-      bottomNavigationBar: const NavigationAppBar(),
+    return const Scaffold(
+      body: RestaurantInfo(imageName: 'le_tacos_de_lyon.jpg',),
+      bottomNavigationBar: NavigationAppBar(),
     );
   }
 
