b4fee43c67113e4df344377b735f023865a7c4ac
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-10 15:58:24 +0100

    Add the restaurant's foreign key

--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -1,17 +1,19 @@
 class Meal {
-  final int id = 0;
+  final int id;
   final String imageName;
   final String title;
   final String availability;
   final String description;
   final double price;
+  final int restaurantId;
 
-  Meal(
-      {required this.imageName,
+  Meal({required this.id,
+      required this.imageName,
       required this.title,
       required this.availability,
       required this.description,
-      required this.price});
+      required this.price,
+      required this.restaurantId});
 
   Map<String, dynamic> toMap() {
     return {
@@ -20,12 +22,13 @@ class Meal {
       'title': title,
       'availability': availability,
       'description' : description,
-      'price' : price
+      'price' : price ,
+      'restaurant_id' : restaurantId
     };
   }
 
   @override
   String toString() {
-    return 'Meal{id: $id, imageName: $imageName, title: $title, availability: $availability, description: $description, price: $price}';
+    return 'Meal{id: $id, imageName: $imageName, title: $title, availability: $availability, description: $description, price: $price, restaurantId: $restaurantId}';
   }
 }
