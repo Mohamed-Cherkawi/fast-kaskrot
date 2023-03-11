1db8307936f3cf2519d2cb112270bfac4cc7e256
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-11 15:38:46 +0100

    Refactored code

--- lib/service/meal_service.dart
+++ lib/service/meal_service.dart
@@ -5,29 +5,38 @@ import '../config/sql_db_config.dart';
 
 class MealService {
   SqlDb database = SqlDb();
+  final String tableName = 'meal';
 
   Future<void> insertMeal(Meal meal) async {
-    final int response = await database.insertData('meal', meal.toMap());
+    final int response = await database.insertData(tableName, meal.toMap());
     if(response != 0) {
       debugPrint('$meal has been saved successfully');
     }
   }
 
   Future<List<Meal>> meals() async {
-    // Get a reference to the database.
-    // Query the table for all The Dogs.
-    final List<Map<String, dynamic>> maps = await database.queryData('meal');
+    final List<Map<String, dynamic>> maps = await database.queryData(tableName);
 
-    // Convert the List<Map<String, dynamic> into a List<Dog>.
+    // Convert the List<Map<String, dynamic> into a List<Meal>.
+    return _listGenerator(maps);
+  }
+
+  Future<List<Meal>> getMealsByRestaurantId(int id) async {
+    final List<Map<String, dynamic>> maps = await database.queryDataById(tableName, id);
+
+    return _listGenerator(maps);
+  }
+
+  List<Meal> _listGenerator(List<Map<String, dynamic>> maps) {
     return List.generate(maps.length, (i) {
       return Meal(
-        restaurantId: maps[i]['restaurant_id'],
-        id: maps[i]['id'],
-        imageName: maps[i]['image_name'],
-        title: maps[i]['title'],
-        availability: maps[i]['availability'],
-        description: maps[i]['description'],
-        price: maps[i]['price'],
+      id: maps[i]['id'],
+      imageName: maps[i]['image_name'],
+      title: maps[i]['title'],
+      availability: maps[i]['availability'],
+      description: maps[i]['description'],
+      price: maps[i]['price'],
+      restaurantId: maps[i]['restaurant_id']
       );
     });
   }
