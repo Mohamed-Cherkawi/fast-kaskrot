4c7b26acd72821e435475e04fd6fe949975ef08b
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-12 23:44:41 +0100

    Prepared the sql script needed to create the database and the tables , besides filling them with data

--- lib/config/sql_db_config.dart
+++ lib/config/sql_db_config.dart
@@ -27,42 +27,46 @@ class SqlDb {
 
   Future<void> onCreate(Database db , int version) async {
     await db.execute('''
-    CREATE TABLE "meal" (
-      "id" INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,
-      "image_name" TEXT NOT NULL,
-      "title" TEXT NOT NULL,
-      "availability" TEXT NOT NULL,
-      "description" TEXT NOT NULL,
-      "price" REAL NOT NULL,
-      "restaurant_id" INTEGER NOT NULL
+    CREATE TABLE meal (
+      id INTEGER  NOT NULL,
+      image_name TEXT NOT NULL,
+      title TEX NOT NULL,
+      availability TEXT NOT NULL,
+      description TEXT NOT NULL,
+      price REAL NOT NULL,
+      restaurant_id INTEGER NOT NULL
+      );
+      
+      CREATE TABLE restaurant (
+      id INTEGER  NOT NULL,
+      name TEXT NOT NULL,
+      address TEXT NOT NULL,
+      image_name TEXT NOT NULL,
+      rating TEXT NOT NULL
+      );
+      
+      INSERT INTO restaurant (id,image_name,rating,name,address) VALUES (1,'le_tacos_de_lyon.jpg','4.3','Tacos de lyon','Marrakesh 4000 , Palestinian road');
+      INSERT INTO restaurant (id,image_name,rating,name,address) VALUES (2,'mc-donalds.jpg','5.5','Mac Donald\'s','Marrakesh 4000 , Sidi Abad');
+      INSERT INTO restaurant (id,image_name,rating,name,address) VALUES (3,'bawlo.jpg','3.4','Bawlo','Marrakesh 4000 , Jamaa El Fena');
+      INSERT INTO restaurant (id,image_name,rating,name,address) VALUES (4,'pizza-hut.jpg','5.2','Pizza Hut','Marrakesh 4000 , Casablanca road , Diour Marjane');
+      
+      INSERT INTO meal (id,image_name,title,availability,description,price,restaurant_id) VALUES (1,'tacos-nuggets.png','Tacos Nuggets','lunch,dinner','Massive nuggets tacos ,with \npreferred sauce + 1 fries',35.00,1);
+      INSERT INTO meal (id,image_name,title,availability,description,price,restaurant_id) VALUES (2,'tacos-xxl.png','Tacos XXL','lunch,dinner','Massive giant tacos ,with \npreferred sauce + 2 fries',55.00,1);
+      INSERT INTO meal (id,image_name,title,availability,description,price,restaurant_id) VALUES (3,'big-tasty.jpg','Big Tasty','lunch,dinner','Delicious Tasty Hamburger,\n + 1 fries + 1 soda',45.00,2);
+      INSERT INTO meal (id,image_name,title,availability,description,price,restaurant_id) VALUES (4,'mac-flurry.png','Mac Flurry','breakfast,lunch,dinner','Fresh ice cream\n eminem,kitkat,twix',20.00,2);
+      INSERT INTO meal (id,image_name,title,availability,description,price,restaurant_id) VALUES (5,'happy-meal.png','Happy Meal','lunch,dinner','Incredible hamburger for childrens,\n+1 fries +1 soda +1 game',30.00,2);
     ''');
-    await db.execute('''
-    CREATE TABLE "restaurant" (
-      "id" INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,
-      "name" TEXT NOT NULL,
-      "address" TEXT NOT NULL,
-      "image_name" TEXT NOT NULL,
-      "rating" TEXT NOT NULL
-      )
-      ''');
-    await db.execute('''
-    ALTER TABLE meal
-      ADD CONSTRAINT FK_meal_restaurant
-      FOREIGN KEY (restaurant_id)
-      REFERENCES restaurant(id)
-      ''');
+    // await db.execute('''
+    // ALTER TABLE meal
+    //   ADD CONSTRAINT FK_meal_restaurant
+    //   FOREIGN KEY (restaurant_id)
+    //   REFERENCES restaurant(id);
+    //   ''');
+
     debugPrint('============ Create Database and Tables ================');
   }
 
   Future<void> onUpgrade(Database database , int oldVersion , int newVersion) async {
-    //RestaurantService restaurantService = RestaurantService();
-    // restaurantService.insertRestaurant(Restaurant(id: 1,imageName: 'le_tacos_de_lyon.jpg', rating: '4.3', name: "Tacos de lyon", address: "Marrakesh 4000 , Palestinian road"));
-    // restaurantService.insertRestaurant(Restaurant(id: 2,imageName: 'mc-donalds.jpg', rating: '5.5', name: "Mac Donald's", address: "Marrakesh 4000 , Sidi Abad"));
-    // restaurantService.insertRestaurant(Restaurant(id: 4,imageName: 'bawlo.jpg', rating: '3.4', name: "Bawlo", address: "Marrakesh 4000 , Jamaa El Fena"));
-    // restaurantService.insertRestaurant(Restaurant(id: 3,imageName: 'pizza-hut.jpg', rating: '5.2', name: "Pizza Hut", address: "Marrakesh 4000 , Casablanca road , Diour Marjane"));
-    
-    // MealService mealService = MealService();
-    //
     // mealService.insertMeal(
     //     Meal(id: 2 , imageName: 'tacos-nuggets.png',title: 'Tacos Nuggets' , availability: 'lunch,dinner' , description: 'Massive nuggets tacos ,with \npreferred sauce + 1 fries',price: 35.00 , restaurantId: 1)
     // );
@@ -70,13 +74,13 @@ class SqlDb {
     //     Meal(id: 3 , imageName: 'tacos-xxl.png',title: 'Tacos XXL' , availability: 'lunch,dinner' , description: 'Massive giant tacos ,with \npreferred sauce + 2 fries',price: 55.00 , restaurantId: 1)
     // );
     // mealService.insertMeal(
-    //     Meal(id: 4 , imageName: 'big-tasty.jpg',title: 'Big Tasty' , availability: 'lunch,dinner' , description: 'Delicious Tasty Hamburger, + 1 fries + 1 soda',price: 45.00 , restaurantId: 2)
+    //     Meal(id: 4 , imageName: 'big-tasty.jpg',title: 'Big Tasty' , availability: 'lunch,dinner' , description: 'Delicious Tasty Hamburger,\n + 1 fries + 1 soda',price: 45.00 , restaurantId: 2)
     // );
     // mealService.insertMeal(
-    //     Meal(id: 5 , imageName: 'mac-flurry.png',title: 'Mac Flurry' , availability: 'breakfast,lunch,dinner' , description: 'Fresh Coled ice cream with eminem,kitkat,twix',price: 20.00 , restaurantId: 2)
+    //     Meal(id: 5 , imageName: 'mac-flurry.png',title: 'Mac Flurry' , availability: 'breakfast,lunch,dinner' , description: 'Fresh ice cream\n eminem,kitkat,twix',price: 20.00 , restaurantId: 2)
     // );
     // mealService.insertMeal(
-    //     Meal(id: 6 , imageName: 'happy-meal.png',title: 'Happy Meal' , availability: 'lunch,dinner' , description: 'Incrdible hamburger meal for childrens ,+1 fries +1 soda +1 game ',price: 30.00 , restaurantId: 2)
+    //     Meal(id: 6 , imageName: 'happy-meal.png',title: 'Happy Meal' , availability: 'lunch,dinner' , description: 'Incredible hamburger for childrens,\n+1 fries +1 soda +1 game',price: 30.00 , restaurantId: 2)
     // );
     debugPrint("=================  OnUpgrade called =================");
   }
