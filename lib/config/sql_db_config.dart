d7892de7a3c5b6df75b4faf0d138a722591e78be
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-13 14:31:46 +0100

    Prepared the onCreate method to be able to create tables and data ( not finished yet ) & refactored code

--- lib/config/sql_db_config.dart
+++ lib/config/sql_db_config.dart
@@ -18,43 +18,62 @@ class SqlDb {
   Future<Database> get db async {
     return _db ??= await initialDb();
   }
-
+  Future<String> getDatabasePath() async {
+    return join(await getDatabasesPath() , 'kaskrot.db');
+  }
   Future<Database> initialDb() async {
     WidgetsFlutterBinding.ensureInitialized();
-    final String path = join(await getDatabasesPath() , 'kaskrot.db');
-    return await openDatabase(path,onCreate: onCreate , version: 19 , onUpgrade: onUpgrade);
+    return await openDatabase(await getDatabasePath(),onCreate: onCreate , version: 31 , onUpgrade: onUpgrade);
   }
 
   Future<void> onCreate(Database db , int version) async {
     await db.execute('''
-    CREATE TABLE meal (
-      id INTEGER  NOT NULL,
-      image_name TEXT NOT NULL,
-      title TEX NOT NULL,
-      availability TEXT NOT NULL,
-      description TEXT NOT NULL,
-      price REAL NOT NULL,
-      restaurant_id INTEGER NOT NULL
-      );
-      
-      CREATE TABLE restaurant (
-      id INTEGER  NOT NULL,
-      name TEXT NOT NULL,
-      address TEXT NOT NULL,
-      image_name TEXT NOT NULL,
-      rating TEXT NOT NULL
-      );
-      
-      INSERT INTO restaurant (id,image_name,rating,name,address) VALUES (1,'le_tacos_de_lyon.jpg','4.3','Tacos de lyon','Marrakesh 4000 , Palestinian road');
-      INSERT INTO restaurant (id,image_name,rating,name,address) VALUES (2,'mc-donalds.jpg','5.5','Mac Donald\'s','Marrakesh 4000 , Sidi Abad');
-      INSERT INTO restaurant (id,image_name,rating,name,address) VALUES (3,'bawlo.jpg','3.4','Bawlo','Marrakesh 4000 , Jamaa El Fena');
-      INSERT INTO restaurant (id,image_name,rating,name,address) VALUES (4,'pizza-hut.jpg','5.2','Pizza Hut','Marrakesh 4000 , Casablanca road , Diour Marjane');
-      
-      INSERT INTO meal (id,image_name,title,availability,description,price,restaurant_id) VALUES (1,'tacos-nuggets.png','Tacos Nuggets','lunch,dinner','Massive nuggets tacos ,with \npreferred sauce + 1 fries',35.00,1);
-      INSERT INTO meal (id,image_name,title,availability,description,price,restaurant_id) VALUES (2,'tacos-xxl.png','Tacos XXL','lunch,dinner','Massive giant tacos ,with \npreferred sauce + 2 fries',55.00,1);
-      INSERT INTO meal (id,image_name,title,availability,description,price,restaurant_id) VALUES (3,'big-tasty.jpg','Big Tasty','lunch,dinner','Delicious Tasty Hamburger,\n + 1 fries + 1 soda',45.00,2);
-      INSERT INTO meal (id,image_name,title,availability,description,price,restaurant_id) VALUES (4,'mac-flurry.png','Mac Flurry','breakfast,lunch,dinner','Fresh ice cream\n eminem,kitkat,twix',20.00,2);
-      INSERT INTO meal (id,image_name,title,availability,description,price,restaurant_id) VALUES (5,'happy-meal.png','Happy Meal','lunch,dinner','Incredible hamburger for childrens,\n+1 fries +1 soda +1 game',30.00,2);
+    CREATE TABLE "meal" (
+      "id" INTEGER  NOT NULL,
+      "image_name" TEXT NOT NULL,
+      "title" TEXT NOT NULL,
+      "availability" TEXT NOT NULL,
+      "description" TEXT NOT NULL,
+      "price" REAL NOT NULL,
+      "restaurant_id" INTEGER NOT NULL
+      )
+      ''');
+    await db.execute('''
+      CREATE TABLE "restaurant" (
+      "id" INTEGER NOT NULL,
+      "name" TEXT NOT NULL,
+      "address" TEXT NOT NULL,
+      "image_name" TEXT NOT NULL,
+      "rating" TEXT NOT NULL
+      )
+      ''');
+    await db.execute('''
+      INSERT INTO restaurant (id,image_name,rating,name,address) VALUES (1,'le_tacos_de_lyon.jpg','4.3','Tacos de lyon','Marrakesh 4000 , Palestinian road')
+      ''');
+    await db.execute('''
+      INSERT INTO restaurant (id,image_name,rating,name,address) VALUES (2,'mc-donalds.jpg','5.5','Mac Donald s','Marrakesh 4000 , Sidi Abad')
+      ''');
+    await db.execute('''
+      INSERT INTO restaurant (id,image_name,rating,name,address) VALUES (3,'bawlo.jpg','3.4','Bawlo','Marrakesh 4000 , Jamaa El Fena')
+      ''');
+    await db.execute('''
+      INSERT INTO restaurant (id,image_name,rating,name,address) VALUES (4,'pizza-hut.jpg','5.2','Pizza Hut','Marrakesh 4000 , Casablanca road , Diour Marjane')
+      ''');
+
+    await db.execute('''
+      INSERT INTO meal (id,image_name,title,availability,description,price,restaurant_id) VALUES (1,'tacos-nuggets.png','Tacos Nuggets','lunch,dinner','Massive nuggets tacos ,with \npreferred sauce + 1 fries',35.00,1)
+      ''');
+    await db.execute('''
+      INSERT INTO meal (id,image_name,title,availability,description,price,restaurant_id) VALUES (2,'tacos-xxl.png','Tacos XXL','lunch,dinner','Massive giant tacos ,with \npreferred sauce + 2 fries',55.00,1)
+      ''');
+    await db.execute('''
+      INSERT INTO meal (id,image_name,title,availability,description,price,restaurant_id) VALUES (3,'big-tasty.jpg','Big Tasty','lunch,dinner','Delicious Tasty Hamburger,\n + 1 fries + 1 soda',45.00,2)
+      ''');
+    await db.execute('''
+      INSERT INTO meal (id,image_name,title,availability,description,price,restaurant_id) VALUES (4,'mac-flurry.png','Mac Flurry','lunch,dinner','Fresh ice cream eminem,\nkitkat,twix',20.00,2)
+      ''');
+    await db.execute('''
+      INSERT INTO meal (id,image_name,title,availability,description,price,restaurant_id) VALUES (5,'happy-meal.png','Happy Meal','lunch,dinner','Incredible hamburger for\n childrens,+1(fries,soda,game)',30.00,2)
     ''');
     // await db.execute('''
     // ALTER TABLE meal
@@ -67,22 +86,10 @@ class SqlDb {
   }
 
   Future<void> onUpgrade(Database database , int oldVersion , int newVersion) async {
-    // mealService.insertMeal(
-    //     Meal(id: 2 , imageName: 'tacos-nuggets.png',title: 'Tacos Nuggets' , availability: 'lunch,dinner' , description: 'Massive nuggets tacos ,with \npreferred sauce + 1 fries',price: 35.00 , restaurantId: 1)
-    // );
-    // mealService.insertMeal(
-    //     Meal(id: 3 , imageName: 'tacos-xxl.png',title: 'Tacos XXL' , availability: 'lunch,dinner' , description: 'Massive giant tacos ,with \npreferred sauce + 2 fries',price: 55.00 , restaurantId: 1)
-    // );
-    // mealService.insertMeal(
-    //     Meal(id: 4 , imageName: 'big-tasty.jpg',title: 'Big Tasty' , availability: 'lunch,dinner' , description: 'Delicious Tasty Hamburger,\n + 1 fries + 1 soda',price: 45.00 , restaurantId: 2)
-    // );
-    // mealService.insertMeal(
-    //     Meal(id: 5 , imageName: 'mac-flurry.png',title: 'Mac Flurry' , availability: 'breakfast,lunch,dinner' , description: 'Fresh ice cream\n eminem,kitkat,twix',price: 20.00 , restaurantId: 2)
-    // );
-    // mealService.insertMeal(
-    //     Meal(id: 6 , imageName: 'happy-meal.png',title: 'Happy Meal' , availability: 'lunch,dinner' , description: 'Incredible hamburger for childrens,\n+1 fries +1 soda +1 game',price: 30.00 , restaurantId: 2)
-    // );
     debugPrint("=================  OnUpgrade called =================");
+  }
+  deleteMyDatabase() async {
+    await deleteDatabase(await getDatabasePath());
   }
 
   Future< List< Map < String,Object? > > > selectData(String query) async {
