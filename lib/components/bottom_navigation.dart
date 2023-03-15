43c8419e049e76314edf285e73452bcce7218cb9
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-15 09:16:13 +0100

    Add a navigation back to the home Page

--- lib/components/bottom_navigation.dart
+++ lib/components/bottom_navigation.dart
@@ -1,31 +1,38 @@
+import 'package:fast_kaskrot/service/local_storage_service.dart';
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 
 class NavigationAppBar extends StatelessWidget {
+  LocalStorageService localStorageService = LocalStorageService();
 
-   const NavigationAppBar({super.key});
+    NavigationAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  const GNav(
+    return  GNav(
       backgroundColor: Colors.black,
       color: Colors.white,
-      padding: EdgeInsets.all(20),
+      padding: const EdgeInsets.all(20),
       tabBorderRadius: 2,
       selectedIndex: 0,
       gap: 20,
       activeColor: Colors.white,
       tabs: [
-        GButton(
+         GButton(
           icon: Icons.restaurant,
           text: 'Restaurants',
+          onPressed: (){
+            Navigator.pushNamed(context, '/');
+          },
         ),
         GButton(
           icon: Icons.shopping_cart_outlined,
           text: 'Cart',
+          onPressed: () {
+          },
         ),
-        GButton(
+        const GButton(
           icon: Icons.history_sharp,
           text: 'History',
         ),
