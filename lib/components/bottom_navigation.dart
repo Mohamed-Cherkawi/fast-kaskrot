77b052d480e2e541e92f9ce665637f630a683238
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-15 11:58:08 +0100

    Add a defaultselectedIndex parameter for the bottom nav bar

--- lib/components/bottom_navigation.dart
+++ lib/components/bottom_navigation.dart
@@ -1,12 +1,12 @@
-import 'package:fast_kaskrot/service/local_storage_service.dart';
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 
 class NavigationAppBar extends StatelessWidget {
-  LocalStorageService localStorageService = LocalStorageService();
+  final int defaultSelectedIndex;
 
-    NavigationAppBar({super.key});
+
+  const NavigationAppBar({super.key ,required this.defaultSelectedIndex});
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +15,7 @@ class NavigationAppBar extends StatelessWidget {
       color: Colors.white,
       padding: const EdgeInsets.all(20),
       tabBorderRadius: 2,
-      selectedIndex: 0,
+      selectedIndex: defaultSelectedIndex,
       gap: 20,
       activeColor: Colors.white,
       tabs: [
