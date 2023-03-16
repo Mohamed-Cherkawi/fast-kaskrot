24ea7c9f7e3488c862483dc196be9e9d4a3706f0
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-16 16:16:36 +0100

    Add the pushed notification functionality when clicking on the order button & code refactoring

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,19 +1,58 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:fast_kaskrot/components/app_bar.dart';
 import 'package:fast_kaskrot/components/bottom_navigation.dart';
 import 'package:fast_kaskrot/components/cart/cart_item.dart';
 import 'package:fast_kaskrot/models/meal.dart';
 import 'package:fast_kaskrot/service/meal_service.dart';
+import 'package:fast_kaskrot/service/order_service.dart';
 import 'package:flutter/material.dart';
 
 class CartPage extends StatefulWidget {
-  List<Meal> meals = [];
-   CartPage({super.key});
+   const CartPage({super.key});
 
   @override
   State<CartPage> createState() => _CartPageState();
 }
 
 class _CartPageState extends State<CartPage> {
+  double totalMealsPrice = 0;
+  Map<int,double> mealsPrices = {};
+  bool isButtonHidden = true;
+  OrderService orderService = OrderService();
+  MealService mealService = MealService();
+
+  void refreshPageState() {
+    setState(() {
+      print('refresh called');
+      mealsPrices.clear();
+    });
+  }
+  void updatePriceById(int id , double newPrice){
+      mealsPrices[id] = newPrice;
+      print(mealsPrices);
+  }
+  void getCartMeals() async{
+   List<Meal> fetchedMeals = await mealService.getAllCartMeal();
+   if(fetchedMeals.isNotEmpty){
+      showButton();
+   }
+  }
+  void hideButton() {
+      setState(() {
+        isButtonHidden = true;
+      });
+  }
+  void showButton(){
+    setState(() {
+      isButtonHidden = false;
+    });
+  }
+  @override
+  void initState() {
+    super.initState();
+    getCartMeals();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,16 +68,26 @@ class _CartPageState extends State<CartPage> {
               child: Column(
                 children: [
                   FutureBuilder(
-                      future: MealService().getAllCartMeal(),
-                      builder: ( context ,  snapshot) {
+                      future: mealService.getAllCartMeal(),
+                      builder: ( context ,  snapshot ) {
                           if(snapshot.hasData){
                             if(snapshot.data!.isEmpty){
-                              return const Text("No Items added to cart");
+                              return const Text(
+                                "No Items Added To Cart !" ,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold ,
+                                    letterSpacing: 1
+                                )
+                              );
                             }
+                            for (Meal meal in snapshot.data!) {
+                             mealsPrices[meal.id] = meal.price;
+                            }
+                            print(mealsPrices);
                             return Column(
                               children: [
                                 for(Meal meal in snapshot.data!)
-                                  CartItem(meal: meal),
+                                  CartItem(meal: meal , refreshParentHolderState: refreshPageState , updatePriceById: updatePriceById),
                               ],
                             );
                           }else if(snapshot.hasError){
@@ -47,6 +96,38 @@ class _CartPageState extends State<CartPage> {
                           return const CircularProgressIndicator();
                       }
                   ),
+                  const SizedBox(height: 20),
+                  if(! isButtonHidden)
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(370, 60),
+                        backgroundColor: Colors.black,
+                        elevation: 0,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50))
+                        )
+                      ),
+                      onPressed: () async{
+                        mealsPrices.values.forEach((price) {
+                            totalMealsPrice += price;
+                        });
+                        debugPrint(totalMealsPrice.toString());
+
+                        await orderService.createOrder(totalMealsPrice);
+
+                        totalMealsPrice = 0;
+
+                        hideButton();
+                        triggerNotification();
+                      },
+                      child: const Text(
+                        'Order Now' ,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold ,
+                            letterSpacing: 4
+                        )
+                      )
+                  )
                 ],
               )
           ),
@@ -56,5 +137,14 @@ class _CartPageState extends State<CartPage> {
       bottomNavigationBar: const NavigationAppBar(defaultSelectedIndex: 1),
     );
   }
-
+  void triggerNotification(){
+    AwesomeNotifications().createNotification(
+        content: NotificationContent(
+            id: 50,
+            channelKey: 'chanelKey',
+            title: 'New Order',
+            body: 'Your order has been successfully placed'
+        )
+    );
+  }
 }
